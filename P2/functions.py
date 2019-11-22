d46605d4549b43e042b3ed68dd3fe660371fac34
LukasGardberg <lukasgardberg2@gmail.com>
2019-11-22 15:45:29 +0100

    visualiser, not working

--- P2/functions.py
+++ P2/functions.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy import exp, sparse
 from scipy.sparse.linalg import spsolve
+from scipy.linalg import norm
+import matplotlib.pyplot as plt
+
 
 def twopBVP(fvec, alpha, beta, L, N):
     # Two point boundry value problem solver.
@@ -31,8 +34,41 @@ def twopBVP(fvec, alpha, beta, L, N):
 
     return y
 
+
 def test_f(x):
     # apply the function f to the array x
     # x assumed to be column np array [[a], [b]...]
 
     return exp(-x)
+
+
+def exact_f(x, alpha, beta, L):
+    return exp(-x) + alpha - 1 + (x/L) * (beta - alpha + 1 - exp(-L))
+
+
+def vshBvp(alpha, beta, length, N):
+    errors = np.array([])
+    step_sizes = np.array([])
+
+    for k in N:
+        print(length)
+        print(k)
+        step_size = length / (k + 1)
+        x = np.linspace(step_size, length - step_size, k)
+        fvec = test_f(x)
+
+        y = twopBVP(fvec, alpha, beta, length, k)
+
+        x = np.insert(x, 0, 0)
+        x = np.append(x, length)
+
+        y_exact = exact_f(x, alpha, beta, length)
+
+        err = norm(y[-2]-y_exact[-2])
+
+        errors = np.append(errors, err)
+        step_sizes = np.append(step_sizes, step_size)
+
+    plt.loglog(step_sizes, errors)
+    plt.grid()
+    plt.show()
--- P2/test.py
+++ P2/test.py
@@ -1,7 +1,8 @@
 from P2 import functions as f
 import numpy as np
 import matplotlib.pyplot as plt
-s = "hej"
+from scipy.linalg import norm
+
 L = 10
 
 # Interior grid points
@@ -9,14 +10,15 @@ N = 100
 alpha = 1
 beta = 2
 
-points = np.linspace(L/(N+1), L - L/(N+1), N)
+# points = np.linspace(L/(N+1), L - L/(N+1), N)
 
-fvec = f.test_f(points)
+# fvec = f.test_f(points)
 
-y = f.twopBVP(fvec, alpha, beta, L, N)
+# y = f.twopBVP(fvec, alpha, beta, L, N)
 
-points = np.insert(points, 0, 0)
-points = np.append(points, L)
+# points = np.insert(points, 0, 0)
+# points = np.append(points, L)
 
-plt.plot(points, y)
-plt.show()
+K = np.arange(1,10)
+K = 2**K
+f.vshBvp(alpha, beta, L, K)
