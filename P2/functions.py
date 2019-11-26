0b69cb3c2ff7f066fc156b7c49d801fc5ba1d9ef
LukasGardberg <lukasgardberg2@gmail.com>
2019-11-26 13:38:28 +0100

    First draft of visualizer

--- P2/functions.py
+++ P2/functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import exp, sparse,sin,cos ,pi
 from scipy.sparse.linalg import spsolve
-from scipy.linalg import norm
+from scipy.linalg import norm, eigh_tridiagonal
 import matplotlib.pyplot as plt
 
 
@@ -41,6 +41,7 @@ def test_f(x):
 
     return exp(-x)
 
+
 def test_fsin(x):
     # apply function fsin to array x
     # x assumed to be column np array
@@ -71,7 +72,7 @@ def vshBvp(alpha, beta, length, N):
 
         y_exact = exact_f(x, alpha, beta, length)
 
-        err = norm(y[-2]-y_exact[-2])
+        err = norm(y-y_exact) / np.sqrt(k + 1)
 
         errors = np.append(errors, err)
         step_sizes = np.append(step_sizes, step_size)
@@ -117,3 +118,52 @@ def beamSolve(fvec, L, N):
     # Use solver to get solution for u
     u = twopBVP(fvec, u0, u1, L, N)
     return u
+
+
+def sv_solver(alpha, beta, L, N):
+    # Input: values of
+    # N: Number of internal points
+
+    # Returns: Eigenvalues and eigenfunctions for the operator
+
+    dx = L / (N + 1)
+
+    subp = (1/(dx * dx)) * np.ones(N - 1)
+    mid = (1/(dx * dx)) * np.ones(N) * (-2)
+
+    eigs, eig_vecs = eigh_tridiagonal(mid, subp)
+
+    # Add boundry values to solutions
+    left_bvs = np.ones(N) * alpha
+    right_bvs = np.ones(N) * beta
+
+    eig_vecs = np.vstack((left_bvs, eig_vecs))
+    eig_vecs = np.vstack((eig_vecs, right_bvs))
+
+    return eigs, eig_vecs
+
+
+def sturm_lv_vis():
+    # Plots error vs N for three first eigenvalues
+    L = 1
+    K = np.arange(2, 5)
+    K = 2**K
+
+    err = np.zeros((10, 3))
+
+    n = 0
+    # For each number of points, calculate the eigenvalues
+    for N in K:
+        eigs, _ = sv_solver(0, 0, L, N)
+        eigs = eigs[:3]
+
+        # Find the error for each eigenvalue
+        for i in range(3):
+            err[n, i] = norm(eigs[i] - (i * pi)**2)
+
+        n = n + 1
+
+    for i in range(3):
+        plt.figure(i)
+        plt.plot(K, err[i, :])
+        plt.show()
--- P2/test.py
+++ P2/test.py
@@ -3,49 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import norm
 
-L = 10
+# L = 10
+#
+# # Interior grid points
+# N = 100
+# alpha = 1
+# beta = 2
+#
+# points = np.linspace(L/(N+1), L - L/(N+1), N)
+# fvec = f.test_fsin(points)
+# yexact = f.exact_sin(points, alpha, beta , L)
+#
+# y = f.twopBVP(fvec, alpha, beta, L, N)
+#
+# yexact = np.insert(yexact,0,alpha)
+# yexact = np.append(yexact,beta)
+#
+# points = np.insert(points, 0, 0)
+# points = np.append(points, L)
+#
+# plt.figure(1)
+# plt.plot(points,y,'ob')
+# plt.show()
+# plt.plot(points,yexact,'or')
+# plt.show()
+# #K = np.arange(1,10)
+# #K = 2**K
+# #f.vshBvp(alpha, beta, L, K)
+#
+# #%% beam deflection section
+# N = 999
+# L = 10
+# points = np.linspace(L/(N+1), L - L/(N+1), N)
+#
+# fvec = f.func_q(points)
+#
+# u = f.beamSolve(fvec, L, N)
+#
+# # Get beam midpoint
+# print(np.median(u))
+# points = np.insert(points, 0, 0)
+# points = np.append(points, 10)
+#
+# plt.plot(points, u)
+# plt.show()
+#
+# #%% section 2
 
-# Interior grid points
+alpha = 0
+beta = 0
+L = 1
 N = 100
-alpha = 1
-beta = 2
 
-points = np.linspace(L/(N+1), L - L/(N+1), N)
-fvec = f.test_fsin(points)
-yexact = f.exact_sin(points, alpha, beta , L)
+# eigs, eig_vecs = f.sv_solver(alpha, beta, L, N)
 
-y = f.twopBVP(fvec, alpha, beta, L, N)
+f.sturm_lv_vis()
 
-yexact = np.insert(yexact,0,alpha)
-yexact = np.append(yexact,beta)
-
-points = np.insert(points, 0, 0)
-points = np.append(points, L)
-
-plt.figure(1)
-plt.plot(points,y,'ob')
-plt.show()
-plt.plot(points,yexact,'or')
-plt.show()
-#K = np.arange(1,10)
-#K = 2**K
-#f.vshBvp(alpha, beta, L, K)
-
-#%% beam deflection section
-N = 999
-L = 10
-points = np.linspace(L/(N+1), L - L/(N+1), N)
-
-fvec = f.func_q(points)
-
-u = f.beamSolve(fvec,L,N)
-
-# Get beam midpoint
-print(np.median(u))
-points = np.insert(points,0,0)
-points = np.append(points,10)
-
-plt.plot(points,u)
-plt.show()
-
-#%% section 2
