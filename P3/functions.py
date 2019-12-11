4c84659d55c246236b999770ca95906bedd472ea
LukasGardberg <lukasgardberg2@gmail.com>
2019-12-11 15:16:21 +0100

    Did part 3

--- P3/functions.py
+++ P3/functions.py
@@ -10,6 +10,7 @@ def eulerstep(Tdx, yold, dt):
     # yold np array
     return yold + dt * Tdx.dot(yold)
 
+
 def TRstep(Tdx, yold, dt):
     a = yold + dt/2 * Tdx.dot(yold)
 
@@ -17,6 +18,7 @@ def TRstep(Tdx, yold, dt):
     b = sparse.diags([mid], [0]) - Tdx * dt/2
 
     return spsolve(b.tocsc(), a)
+
 
 def LaxWenStep(uold, amu):
     size = np.size(uold)
@@ -31,8 +33,6 @@ def LaxWenStep(uold, amu):
 
     Tdx = sparse.diags(diagonals, [-(size - 1), -1, 0, 1, size - 1])
 
-    
-
     return Tdx.dot(uold)
 
 
@@ -42,7 +42,7 @@ def diffSolve(N, M, L, T, fvec):
     dx = L / (N + 1)
     dt = T / M
 
-    print(dt/(dx*dx))
+    # print(dt/(dx*dx))
 
     subp = np.ones(N - 1)
     mid = np.ones(N) * (-2)
@@ -81,6 +81,10 @@ def startG(x):
     return np.exp(-100*(x-0.5)**2)
 
 
+def startF(x):
+    return -x**2 + x
+
+
 def advecSolve(N, M, g, a):
     T = 5
 
@@ -110,3 +114,67 @@ def advecSolve(N, M, g, a):
     print(amu)
     return sol
 
+
+def convdifstep(uold, Tdx, Sdx, a, d, dt):
+
+    diff = d * Tdx - a * Sdx
+
+    unit = sparse.diags([np.ones(len(uold))], [0])
+
+    lhs = unit - (dt/2) * diff
+    rhs = unit + (dt/2) * diff
+
+    return spsolve(lhs, rhs.dot(uold))
+
+
+def convdifsolve(N, M, g, a, d):
+    # Solver for convection-diffusion equation
+    # Assume x \in [0, 1], t \in [0, 1]
+
+    dx = 1 / N
+    dt = 1 / M
+
+    # Create Tdx (diffusion)
+    subp = np.ones(N - 1)
+    mid = np.ones(N) * (-2)
+
+    bottop = np.ones(1)
+
+    diagsT = [bottop, subp, mid, subp, bottop]
+
+    Tdx = 1 / (dx * dx) * sparse.diags(diagsT, [-(N-1), -1, 0, 1, (N-1)])
+
+    # Create Sdx (advection)
+    sub = -np.ones(N - 1)
+    sup = np.ones(N - 1)
+
+    bot = np.ones(1)
+    top = -np.ones(1)
+
+    diagsS = [bot, sub, sup, top]
+
+    Sdx = 1 / (2 * dx) * sparse.diags(diagsS, [-(N - 1), -1, 1, (N - 1)])
+
+    # Find startvalues
+    xx = np.linspace(0, 1, N + 1)
+    uold = g(xx)
+    uold = np.delete(uold, -1)
+
+    # Create solution matrix
+    sol = np.zeros((M + 1, N))
+
+    sol[0, :] = uold
+
+    for i in range(1, M + 1):
+        unew = convdifstep(uold, Tdx, Sdx, a, d, dt)
+
+        sol[i, :] = unew
+
+        uold = unew
+
+    # Add first column as last, periodic boundry
+    sol = np.hstack((sol, np.reshape(sol[:, 0], (M + 1, 1))))
+
+    print(f"Mesh Pelcet = {abs(a/d) * dx}")
+
+    return sol
--- P3/test.py
+++ P3/test.py
@@ -31,9 +31,9 @@ ax.set_title('Surface plot')
 plt.show()
 
 #%% del 2
-a = -0.29
-N = 200
-M = 400
+a = 0.096
+N = 100
+M = 60
 
 
 xx = np.linspace(0, 1, N + 1)
@@ -42,8 +42,8 @@ tt = np.linspace(0, 5, M + 1)
 fig1 = plt.figure(1)
 ax = fig1.add_subplot(111, projection='3d')
 
-sol = f.advecSolve(N,M,f.startG,a)
-xx, tt = np.meshgrid(xx,tt)
+sol = f.advecSolve(N, M, f.startG, a)
+xx, tt = np.meshgrid(xx, tt)
 
 ax.plot_surface(tt, xx, sol, cmap='viridis', edgecolor='none')
 ax.set_title('Surface plot')
@@ -52,12 +52,31 @@ plt.show()
 err = np.zeros(M+1)
 t = np.linspace(0, 5, M + 1)
 for i in range(M+1):
-    err[i] = 1/np.sqrt(M+1) * norm(sol[i,:])
+    err[i] = 1/np.sqrt(N) * norm(sol[i,:])
 
 fig2 = plt.figure(2)
-plt.plot(t,err)
+plt.plot(t, err)
 plt.show()
 
+#%% Del 3
 
+# Points in space
+N = 100
+# Points in time
+M = 1000
 
+a = 1
+d = 0.1
 
+xx = np.linspace(0, 1, N + 1)
+tt = np.linspace(0, 5, M + 1)
+
+fig1 = plt.figure(1)
+ax = fig1.add_subplot(111, projection='3d')
+
+sol = f.convdifsolve(N, M, f.startG, a, d)
+xx, tt = np.meshgrid(xx, tt)
+
+ax.plot_surface(tt, xx, sol, cmap='viridis', edgecolor='none')
+ax.set_title('Surface plot')
+plt.show()
